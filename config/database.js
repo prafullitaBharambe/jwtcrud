c9da254ef5f04d1cff68146e1766ea8f3f7a1dc4
prafullitaBharambe <106234183+prafullitaBharambe@users.noreply.github.com>
2022-06-06 11:27:34 +0530

    second commit

--- config/database.js
+++ config/database.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const { MONGO_URI } = process.env;
+
+exports.connect = () => {
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("DB Connected");
+    })
+    .catch((err) => {
+      console.log("Error Connetcing db"+err);
+    });
+};
